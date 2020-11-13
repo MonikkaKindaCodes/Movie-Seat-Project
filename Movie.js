9ed1f41361da56e7b9e38f949ad985145ce9eb40
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2020-11-12 21:13:17 -0600

    Add files via upload

--- Movie.js
+++ Movie.js
@@ -6,9 +6,15 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
-//Updated total and count
+//Update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected'); 
+
+    //Copy selected seats into an array
+    //Map through array
+    //Returing new array of indexes.
+
+    const seatsIndex = [...selectedSeats]
     
     const selectedSeatsCount = selectedSeats.length;
     
